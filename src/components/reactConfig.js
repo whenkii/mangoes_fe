df0e389ef0156132f32cae1cb4494c28d8fd50ba
arunkumarthammichetti@gmail.com <arunkumarthammichetti@gmail.com>
2025-04-10 01:34:17 +0800

    first push

--- src/components/reactConfig.js
+++ src/components/reactConfig.js
@@ -1,6 +1,5 @@
 export const config = {
-  restAPIHost: "7001",
-  restAPIserver: "http://localhost",
+  // restAPIHost: "7001",restAPIserver: "http://localhost",
   restAPIHost:"7001",restAPIserver:"http://www.garden-roots.com"
 };
 
